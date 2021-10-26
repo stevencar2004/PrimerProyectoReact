b7e22635ee9d6bab312eb9eca0b49cdd40297eb6
Sebaspatig <sebaspatig@gmail.com>
2021-10-26 18:17:26 -0500

    correccion de conflictos

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './componentes/styles/index.css';
+import App from './componentes/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
